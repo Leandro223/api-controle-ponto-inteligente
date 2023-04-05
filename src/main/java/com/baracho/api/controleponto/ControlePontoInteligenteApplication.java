5fdd3ffabf4f17f0ea0b14a91a07e66a89a054ff
Baracho <baracholss@gmail.com>
2023-04-05 09:44:18 -0300

    subindo aplicação controle de ponto

---
+++ src/main/java/com/baracho/api/controleponto/ControlePontoInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.baracho.api.controleponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePontoInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baracho/api/controleponto/ControlePontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baracho.api.controleponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
