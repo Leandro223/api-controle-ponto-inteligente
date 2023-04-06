8507f84f30daf53573075ddec7fb8e2eb39f6dc6
Baracho <baracholss@gmail.com>
2023-04-06 09:10:44 -0300

    Adicionado a classe controller de empresa

---
+++ src/main/java/com/baracho/api/controleponto/controllers/EmpresaController.java
@@ -0,0 +1,9 @@
+package com.baracho.api.controleponto.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/empresas")
+public class EmpresaController {
+}
