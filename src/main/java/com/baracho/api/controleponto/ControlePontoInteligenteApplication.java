a30fdba00ea75c1a310501bf14efbe594bfbcbe6
Baracho <baracholss@gmail.com>
2023-04-06 08:45:02 -0300

    Algumas importações incluidas no projeto: mySql, validation e feito DTO de Empresa.

--- src/main/java/com/baracho/api/controleponto/ControlePontoInteligenteApplication.java
+++ src/main/java/com/baracho/api/controleponto/ControlePontoInteligenteApplication.java
@@ -3,7 +3,7 @@ package com.baracho.api.controleponto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //(exclude = { SecurityAutoConfiguration.class })
 public class ControlePontoInteligenteApplication {
 
 	public static void main(String[] args) {
