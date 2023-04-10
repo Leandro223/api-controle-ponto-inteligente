1578e18adeddd99111138d0414cc36aa020cdff9
Baracho <baracholss@gmail.com>
2023-04-10 08:24:03 -0300

    adicionado o cross origin permitindo que api posso ter acesso externo

--- src/main/java/com/baracho/api/controleponto/controllers/EmpresaController.java
+++ src/main/java/com/baracho/api/controleponto/controllers/EmpresaController.java
@@ -8,15 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/empresas")
+@CrossOrigin(origins = "*")
 public class EmpresaController {
 
     private static final Logger log = LoggerFactory.getLogger(EmpresaController.class);
