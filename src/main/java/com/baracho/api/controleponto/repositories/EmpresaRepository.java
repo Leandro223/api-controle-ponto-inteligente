555bd563dec1de7b7774f5824ffa8ae7db9193a3
Baracho <baracholss@gmail.com>
2023-04-11 14:48:46 -0300

    Feito metodo para consulta

--- src/main/java/com/baracho/api/controleponto/repositories/EmpresaRepository.java
+++ src/main/java/com/baracho/api/controleponto/repositories/EmpresaRepository.java
@@ -1,11 +1,18 @@
 package com.baracho.api.controleponto.repositories;
 
 import com.baracho.api.controleponto.entities.Empresa;
+import com.baracho.api.controleponto.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 
     @Transactional(readOnly = true)
     Empresa findByCnpj(String cnpj);
+
+    @Query("SELECT f FROM Funcionario f WHERE f.empresa.cnpj = :cnpj")
+    List<Funcionario> findFuncionariosByCnpj(String cnpj);
 }
