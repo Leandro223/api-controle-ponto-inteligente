b4b18cfcdd2c793a5edfc2e17124dacb43febacc
Baracho <baracholss@gmail.com>
2023-04-13 14:47:39 -0300

    Adicionado anotação @Service

--- src/main/java/com/baracho/api/controleponto/serviceimpl/LancamentoServiceImpl.java
+++ src/main/java/com/baracho/api/controleponto/serviceimpl/LancamentoServiceImpl.java
@@ -8,11 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class LancamentoServiceImpl implements LancamentoService {
       private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
 
