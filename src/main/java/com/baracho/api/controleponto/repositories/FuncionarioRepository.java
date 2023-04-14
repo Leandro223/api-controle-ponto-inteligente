0745c2850f8f9119dce3cd3f6438a3f56f02d7c7
Baracho <baracholss@gmail.com>
2023-04-14 11:09:08 -0300

    Feito query para busca usuario pra autenticação

--- src/main/java/com/baracho/api/controleponto/repositories/FuncionarioRepository.java
+++ src/main/java/com/baracho/api/controleponto/repositories/FuncionarioRepository.java
@@ -2,7 +2,10 @@ package com.baracho.api.controleponto.repositories;
 
 import com.baracho.api.controleponto.entities.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface FuncionarioRepository  extends JpaRepository<Funcionario, Long> {
@@ -12,4 +15,9 @@ public interface FuncionarioRepository  extends JpaRepository<Funcionario, Long>
     Funcionario findByEmail(String email);
 
     Funcionario findByCpfOrEmail(String cpf, String email);
+
+    @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome")
+    Optional<Funcionario> buscarPorNome(String nome);
+
+    //Boolean existsByUsername(String nome);
 }
